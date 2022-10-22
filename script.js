62ddb0fd680d7f2d64d2077fd128359108f9f92d
Mahir Sowad <mahirsowad3@gmail.com>
2022-10-22 19:53:18 -0400

    Display First Number Input (Partially Working)

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 //constants to keep track of what current button press means
-const INPUT_FIRST_NUM = 0;
-const OPERATOR = 1;
-const INPUT_SECOND_NUM = 2;
-const INPUT_INVALID = 3;
+const INPUT_EMPTY = -1;
+
+let firstNumberInput = INPUT_EMPTY;
+let operatorInput = INPUT_EMPTY;
+let secondNumberInput = INPUT_EMPTY;
 
 //event listener wrapper to wait for DOM elemnts to load before continuing
 document.addEventListener("DOMContentLoaded", (e) => {
   //get the number buttons
   let numberButtons = document.getElementsByClassName("number");
-  console.log(numberButtons);
-  console.log(numberButtons.length);
-  for (let i = 0; i < numberButtons.length; i++) {
-    console.log(numberButtons[i]);
-  }
 
   //get the operator buttons
   let operatorButtons = document.getElementsByClassName("operator");
@@ -45,4 +41,41 @@ document.addEventListener("DOMContentLoaded", (e) => {
   for (let i = 0; i < deleteButton.length; i++) {
     console.log(deleteButton[i]);
   }
+
+  //get the results panel displays
+  let previousOperationPanel =
+    document.getElementsByClassName("previous-operation");
+  let currentOperationPanel =
+    document.getElementsByClassName("current-operation");
+
+  for (let i = 0; i < numberButtons.length; i++) {
+    //set up event listeners on each number button
+    numberButtons[i].addEventListener("click", () => {
+      numberButtonClickHandler(numberButtons[i]);
+    });
+  }
+
+  /*
+   function to handle clicks for numberButtons
+  */
+  let numberButtonClickHandler = (button) => {
+    //get the number associated with that button
+    let buttonNumber = button.innerText;
+
+    //check if firstNumberInput is empty
+    if (firstNumberInput === INPUT_EMPTY) {
+      //if empty, then set firstNumberInput to buttonNumber
+      firstNumberInput = buttonNumber;
+      currentOperationPanel[0].innerText = firstNumberInput;
+    } else if (firstNumberInput !== INPUT_EMPTY) {
+      //if firstNumberInput is not empty
+      //then check if operatorInput is empty
+      if (operatorInput === INPUT_EMPTY) {
+        //if operatorInput is empty
+        //then append number to the end of firstNumberInput
+        firstNumberInput = firstNumberInput.concat(buttonNumber);
+        currentOperationPanel[0].innerText = firstNumberInput;
+      }
+    }
+  };
 });
