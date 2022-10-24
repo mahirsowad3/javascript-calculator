88c202d8438d8d2c7cabfdfa63eb60c9db83230c
Mahir Sowad <mahirsowad3@gmail.com>
2022-10-23 20:16:44 -0400

    Clean Up Some Debug Log Statements

--- script.js
+++ script.js
@@ -12,35 +12,15 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
   //get the operator buttons
   let operatorButtons = document.getElementsByClassName("operator");
-  console.log(operatorButtons);
-  console.log(operatorButtons.length);
-  for (let i = 0; i < operatorButtons.length; i++) {
-    console.log(operatorButtons[i]);
-  }
 
   //get the equal button
   let equalButton = document.getElementsByClassName("equal-btn");
-  console.log(equalButton);
-  console.log(equalButton.length);
-  for (let i = 0; i < equalButton.length; i++) {
-    console.log(equalButton[i]);
-  }
 
   //get the clear button
   let clearButton = document.getElementsByClassName("clear-btn");
-  console.log(clearButton);
-  console.log(clearButton.length);
-  for (let i = 0; i < clearButton.length; i++) {
-    console.log(clearButton[i]);
-  }
 
   //get the delete button
   let deleteButton = document.getElementsByClassName("del-btn");
-  console.log(deleteButton);
-  console.log(deleteButton.length);
-  for (let i = 0; i < deleteButton.length; i++) {
-    console.log(deleteButton[i]);
-  }
 
   //get the results panel displays
   let previousOperationPanel =
@@ -48,6 +28,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
   let currentOperationPanel =
     document.getElementsByClassName("current-operation");
 
+  //set up event sisteners on number buttons
   for (let i = 0; i < numberButtons.length; i++) {
     //set up event listeners on each number button
     numberButtons[i].addEventListener("click", () => {
